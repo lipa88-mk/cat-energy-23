f60b1425b1e4ac44a3ac008d3c3db3419d054183
lipa88-mk <lipa88@mail.ru>
2021-08-10 20:31:51 +0300

    добавлены блоки

---
+++ source/js/main.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector(".header__burger");
+const menu = document.querySelector(".header__nav");
+burger.onclick = function() {
+  menu.classList.toggle("header__nav--close");
+  menu.classList.toggle("header__nav--open");
+};
