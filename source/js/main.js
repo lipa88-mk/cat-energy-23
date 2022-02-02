809bf8b581598489b2d2bdef6af72d9f7e2b26f4
lipa88-mk <lipa88@mail.ru>
2022-02-03 00:39:17 +0300

    правит шапку и меню под планшет

--- source/js/main.js
+++ source/js/main.js
@@ -2,5 +2,4 @@ const burger = document.querySelector(".header__burger");
 const menu = document.querySelector(".header__nav");
 burger.onclick = function() {
   menu.classList.toggle("header__nav--close");
-  menu.classList.toggle("header__nav--open");
 };
